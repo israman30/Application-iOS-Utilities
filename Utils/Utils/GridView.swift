1cddc585acc487ad2ff5edffd0b36fd15af61980
@israman30 <you@example.com>
2023-12-30 21:06:49 -0500

    F - added Grid view file

---
+++ Utils/Utils/GridView.swift
@@ -0,0 +1,45 @@
+//
+//  GridView.swift
+//  Utils
+//
+//  Created by Israel Manzo on 12/30/23.
+//
+
+import SwiftUI
+
+struct GridView<Content: View>: View {
+    
+    let columns: Int
+    let content: () -> Content
+    
+    var adaptiveColumns: [GridItem] {
+        Array(repeating: GridItem(.flexible()), count: columns)
+    }
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: adaptiveColumns) {
+                content()
+            }
+        }
+    }
+    
+    init(columns: Int, @ViewBuilder content: @escaping () -> Content) {
+        self.columns = columns
+        self.content = content
+    }
+}
+
+struct GridViewDisplay: View {
+    var body: some View {
+        GridView(columns: 2) {
+            ForEach(0..<10) { index in
+                Text("View: \(index)")
+            }
+        }
+    }
+}
+
+#Preview {
+    GridViewDisplay()
+}
