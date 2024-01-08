f834196efb4475ce87ede5e0af127042fa74ba69
@israman30 <you@example.com>
2024-01-07 20:36:24 -0500

    Fgit add . - created custom viewmodifer

---
+++ Utils/Utils/TextFieldUtils.swift
@@ -0,0 +1,71 @@
+//
+//  TextFieldUtils.swift
+//  Utils
+//
+//  Created by Israel Manzo on 1/7/24.
+//
+
+import SwiftUI
+
+struct TextFieldUtils: View {
+    @State var text = ""
+    var body: some View {
+        VStack {
+            CustomTextField(placeholder: "Enter something", inputText: $text, headerText: "Some Header", iconPlaceholder: "magnifyingglass")
+            
+            
+        }
+        
+        
+    }
+}
+
+#Preview {
+    TextFieldUtils()
+}
+
+struct CustomTextField: View {
+    
+    var placeholder = ""
+    @Binding var inputText: String
+    var font: Font = .title
+    var headerText = ""
+    var iconPlaceholder = ""
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(headerText)
+                .font(.title)
+            HStack {
+                Image(systemName: iconPlaceholder)
+                TextField(placeholder, text: $inputText)
+            }
+            .padding()
+            .font(font)
+            .textFieldStyle(.plain)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.orange]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(20)
+            .shadow(color: .gray, radius: 10)
+        }
+        .padding()
+        
+    }
+    
+}
+
+struct CustomModifier: ViewModifier {
+    
+    var colors: [Color]
+    var startPoint: UnitPoint = .topLeading
+    var endPoint: UnitPoint = .bottomTrailing
+    var cornerRadius: CGFloat = 20
+    var shadowRadius: CGFloat = 10
+    var shadowColor: Color = .gray
+    
+    func body(content: Content) -> some View {
+        content
+            .background(LinearGradient(gradient: Gradient(colors: colors), startPoint: startPoint, endPoint: endPoint))
+            .cornerRadius(cornerRadius  )
+            .shadow(color: .gray, radius: shadowRadius)
+    }
+}
