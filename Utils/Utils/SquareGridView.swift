9b8a1b207a11a601ae090a69200bdc01d9346e43
Israel Manzo <israelmanzo814@gmail.com>
2025-04-05 23:03:42 -0400

    added square grid

--- Utils/Utils/SquareGridView.swift
+++ Utils/Utils/SquareGridView.swift
@@ -7,27 +7,30 @@
 
 import SwiftUI
 
-struct SquareGridView<Content: View, LoaderContent: View, Item: Hashable>: View {
+public struct SquareGridView<Content: View, Item: Hashable>: View {
     var items: [Item]
     var totalCount: Int
     var columns: Int = 3
     var columnSpacing: CGFloat = 2
     var rowSpacing: CGFloat = 2
+    var showsIndicators: Bool = false
     
     let buildItem: (Item) -> Content
-    let loadingItem: () -> LoaderContent
     
-    var body: some View {
+    func adaptiveColumns(_ cellSize: CGFloat) -> [GridItem] {
+        .init(repeating:  GridItem(.fixed(cellSize), spacing: columnSpacing), count: columns)
+    }
+    
+    public var body: some View {
         GeometryReader { proxy in
             let cellSize: CGFloat = proxy.size.width / CGFloat(columns)
-            ScrollView(.vertical, showsIndicators: false) {
-                LazyVGrid(columns: .init(repeating:  GridItem(.fixed(cellSize), spacing: columnSpacing), count: columns), spacing: rowSpacing) {
+            ScrollView(.vertical, showsIndicators: showsIndicators) {
+                LazyVGrid(
+                    columns: adaptiveColumns(cellSize),
+                    spacing: rowSpacing
+                ) {
                     ForEach(items, id: \.self) { item in
                         buildItem(item)
-                            .frame(height: cellSize)
-                    }
-                    if items.count < totalCount {
-                        loadingItem()
                             .frame(height: cellSize)
                     }
                 }
@@ -35,3 +38,32 @@ struct SquareGridView<Content: View, LoaderContent: View, Item: Hashable>: View 
         }
     }
 }
+
+@MainActor
+class SomeModel: ObservableObject {
+    @Published var characters: [Character] = []
+    @Published var totalCount: Int = 50
+    
+    func loadMoreCharacters() {
+        let newCharacters = Array("ABCDEFGHIJKLMOPQRSTUVWXYZ")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.characters = newCharacters
+            self.totalCount += newCharacters.count
+        }
+    }
+}
+struct GridTextView: View {
+    @StateObject var model = SomeModel()
+    var body: some View {
+        SquareGridView(items: model.characters, totalCount: model.totalCount, columns: 3, columnSpacing: 2, rowSpacing: 2) { item in
+                Text("Item: \(item)")
+            }
+            .onAppear {
+                model.loadMoreCharacters()
+            }
+    }
+}
+
+#Preview(body: {
+    GridTextView()
+})
