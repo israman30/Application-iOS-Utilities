a4b6fcf354053331121c866d69f2958e743b475f
Israel Manzo <israelmanzo814@gmail.com>
2025-04-07 23:29:57 -0400

    added new file for form view

---
+++ Utils/Utils/FormView.swift
@@ -0,0 +1,26 @@
+//
+//  FormView.swift
+//  Utils
+//
+//  Created by Israel Manzo on 4/7/25.
+//
+
+import SwiftUI
+
+struct FormView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    FormView()
+}
+
+struct UtilFormView: View {
+    var body: some View {
+        Group {
+            Text("Util Form View")
+        }
+    }
+}
