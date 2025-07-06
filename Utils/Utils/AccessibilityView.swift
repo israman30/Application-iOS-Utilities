b9b4fdba32ddfdb5c6b387ff870dad95adfd9724
Israel Manzo <israelmanzo814@gmail.com>
2025-07-05 21:58:51 -0400

    added accessibility file

---
+++ Utils/Utils/AccessibilityView.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityView.swift
+//  Utils
+//
+//  Created by Israel Manzo on 7/5/25.
+//
+
+import SwiftUI
+
+struct AccessibilityView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AccessibilityView()
+}
