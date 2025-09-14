27b2cb4e581b072fdbcb7f6b9733fb59d3d394e8
Israel Manzo <israelmanzo814@gmail.com>
2025-09-14 00:08:25 -0400

    update tooltip

--- Utils/Utils/TooltipView.swift
+++ Utils/Utils/TooltipView.swift
@@ -8,25 +8,18 @@
 import SwiftUI
 
 struct TooltipView: View {
-    var list: [TooltipModel] = [
-        TooltipModel(title: "Hello World")
-    ]
-    
-    var list2: [TooltipModel] = [
-        TooltipModel(icon: "heart.fill", title: "31")
-    ]
-    
-    var list3: [TooltipModel] = [
-        TooltipModel(icon: "bubble.fill", title: "31")
-    ]
     
     var body: some View {
         VStack(spacing: 50) {
-            Tooltip(items: list, type: .bottom)
-            Tooltip(items: list2, type: .top)
-            Tooltip(items: list3, type: .left)
-            Tooltip(items: list, type: .right)
             ActivityItem("Label")
+            
+            ActivityItem("Label 2") {
+                Image(systemName: "heart.fill")
+            }
+            
+            ActivityItem("12", type: .left) {
+                Image(systemName: "bubble.fill")
+            }
         }
     }
 }
@@ -58,37 +51,32 @@ struct Triangle: Shape {
     }
 }
 
-public struct TooltipModel {
-    let id = UUID().uuidString
-    var icon: String? = nil
-    let title: String
-}
-
 enum TooltipDirection {
     case top, left, right, bottom
 }
 
-public struct ActivityItem: View {
+public struct ActivityItem<Icon: View>: View {
     var title: String
     var type: TooltipDirection = .bottom
-    var icon: String?
+    var icon: Icon
     
-    init(_ title: String, type: TooltipDirection = .bottom, icon: String? = nil) {
+    init(
+        _ title: String,
+        type: TooltipDirection = .bottom,
+        @ViewBuilder icon: () -> Icon = EmptyView.init
+    ) {
         self.title = title
         self.type = type
-        self.icon = icon
+        self.icon = icon()
     }
     
     public var body: some View {
         ZStack(alignment: alignment) {
             HStack {
                 HStack(spacing: 2) {
-                    if let icon = icon {
-                        Image(systemName: icon)
-                            .resizable()
-                            .foregroundStyle(.white)
-                            .frame(width: 16, height: 16)
-                    }
+                    icon
+                        .foregroundStyle(.white)
+                        .padding(.trailing, 2)
                     
                     Text(title)
                         .font(.system(size: 14, weight: .semibold))
@@ -152,81 +140,6 @@ public struct ActivityItem: View {
                 .font(.system(size: 14, weight: .semibold))
                 .lineLimit(1)
                 .foregroundStyle(.white)
-        }
-    }
-}
-
-
-
-
-
-public struct Tooltip: View {
-    var items: [TooltipModel]
-    var type: TooltipDirection
-    
-    public var body: some View {
-        ZStack(alignment: alignment()) {
-            HStack(spacing: 8) {
-                ForEach(items, id: \.id) { item in
-                    ActivityItem(item: item)
-                }
-            }
-            .padding(8)
-            .background(Color.red.opacity(0.5))
-            .cornerRadius(8)
-            
-            switch type {
-            case .top:
-                triangle()
-                    .offset(y: -10)
-            case .left:
-                triangle()
-                    .rotationEffect(.degrees(-90))
-                    .offset(x: -15)
-            case .right:
-                triangle()
-                    .rotationEffect(.degrees(90))
-                    .offset(x: 15)
-            case .bottom:
-                triangle()
-                    .rotationEffect(.degrees(180))
-                    .offset(y: 10)
-            }
-        }
-    }
-    
-    private func ActivityItem(item: TooltipModel) -> some View {
-        HStack(spacing: 2) {
-            if let icon = item.icon {
-                Image(systemName: icon)
-                    .resizable()
-                    .foregroundStyle(.white)
-                    .frame(width: 16, height: 16)
-            }
-            
-            Text(item.title)
-                .font(.system(size: 14, weight: .semibold))
-                .lineLimit(1)
-                .foregroundStyle(.white)
-        }
-    }
-    
-    private func triangle() -> some View {
-        Triangle()
-            .fill(Color.red.opacity(0.5))
-            .frame(width: 20, height: 10)
-    }
-    
-    private func alignment() -> Alignment {
-        switch type {
-        case .top:
-            return .top
-        case .left:
-            return .leading
-        case .right:
-            return .trailing
-        case .bottom:
-            return .bottom
         }
     }
 }
