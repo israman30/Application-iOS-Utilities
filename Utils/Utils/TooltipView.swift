ef5aadaaee520c2d3091d59b9e30b079ffaff9b7
Israel Manzo <israelmanzo814@gmail.com>
2025-09-13 22:39:41 -0400

    added triangle create function

--- Utils/Utils/TooltipView.swift
+++ Utils/Utils/TooltipView.swift
@@ -16,3 +16,26 @@ struct TooltipView: View {
 #Preview {
     TooltipView()
 }
+
+struct Triangle: Shape {
+    nonisolated func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let topMiddle = CGPoint(x: rect.midX, y: rect.minY)
+        let bottomLeft = CGPoint(x: rect.minX, y: rect.maxY)
+        let bottomRight = CGPoint(x: rect.maxX, y: rect.maxY)
+        
+        path.move(to: bottomLeft)
+        path.addLine(to: bottomRight)
+        
+        path.addArc(
+            center: CGPoint(x: topMiddle.x, y: topMiddle.y),
+            radius: 0,
+            startAngle: .degrees(0),
+            endAngle: .degrees(180),
+            clockwise: true
+        )
+        
+        path.addLine(to: bottomLeft)
+        return path
+    }
+}
