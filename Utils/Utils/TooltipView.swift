c31604facd1d1d81eb21576192044defeccfefe5
Israel Manzo <israelmanzo814@gmail.com>
2025-09-13 22:39:01 -0400

    added new file for tooltip

---
+++ Utils/Utils/TooltipView.swift
@@ -0,0 +1,18 @@
+//
+//  TooltipView.swift
+//  Utils
+//
+//  Created by Israel Manzo on 9/13/25.
+//
+
+import SwiftUI
+
+struct TooltipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TooltipView()
+}
